6bbda4786b5e69cc2a2ae40451b44d70886639f4
YerbolIrgaliyev <yerbol.irgaliyev@gmail.com>
2019-02-17 16:27:03 +0600

    Добавьте файлы проекта.

---
+++ OneDimensionArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_17_01
+{
+    public class OneDimensionArray
+    {
+        private int[] numbers;
+        public OneDimensionArray(int size)
+        {
+            numbers = new int[size];
+        }
+        public int this[int index]
+        {
+            get { return numbers[index]; }
+            set { numbers[index] = value * value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_17_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string sizeStr;
+                int sizeCnt;
+
+                Console.WriteLine("\nВведите размер массива: ");
+                sizeStr = Console.ReadLine();
+
+                if (int.TryParse(sizeStr, out sizeCnt))
+                {
+                    OneDimensionArray oneDimensionArray = new OneDimensionArray(sizeCnt);
+                    for(int i = 0; i < sizeCnt;)
+                    {
+                        int numberInt;
+                        string numberStr;
+
+                        Console.WriteLine($"Введите {i + 1} значение: ");
+                        numberStr = Console.ReadLine();
+
+                        if (int.TryParse(numberStr, out numberInt))
+                        {
+                            oneDimensionArray[i] = numberInt;
+                            i++;
+                        }
+                        else { Console.WriteLine("Введены неправильные данные!"); }  
+                    }
+                    Console.WriteLine("Значения массива: ");
+                    for(int i = 0; i < sizeCnt; i++)
+                    {
+                        Console.WriteLine($"{i + 1} : {oneDimensionArray[i]}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Введены неправильные данные!");
+                }
+            }
+        }
+    }
+}
